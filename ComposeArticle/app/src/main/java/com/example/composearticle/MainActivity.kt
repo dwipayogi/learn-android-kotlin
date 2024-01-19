2a87a454cb6068804eb876058df859b3c6aa08e7
Dwipa Yogiswara <dwipayogi50@gmail.com>
2024-01-19 19:49:12 +0700

    finished ComposeArticle

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticleText()
+                    ArticleImage(stringResource(R.string.title), stringResource(R.string.definition), stringResource(R.string.desc))
                 }
             }
         }
@@ -35,37 +38,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArticleText(modifier: Modifier = Modifier) {
-    Text(
-        text = "Jetpack Compose tutorial",
-        fontSize = 24.sp,
-        padding = 16.dp,
-        modifier = modifier.padding(16.dp).fontSize(24.sp)
-    )
-    Text(
-        text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-        modifier = modifier
-    )
-    Text(
-        text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
-        modifier = modifier
-    )
+fun ArticleText(title: String, definition: String , desc: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = definition,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = desc,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
 
 @Composable
-fun ArticleImage(modifier: Modifier = Modifier) {
+fun ArticleImage(title: String, definition: String , desc: String, modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.bg_compose_background)
-    Image(
-        painter = image,
-        contentDescription = "Jetpack Compose",
-        modifier = modifier
-    )
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier
+        )
+        ArticleText(title, definition, desc, modifier)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        ArticleText()
+        ArticleImage(stringResource(R.string.title), stringResource(R.string.definition), stringResource(R.string.desc))
     }
 }
